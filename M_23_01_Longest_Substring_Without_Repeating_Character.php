72733078ee92d9d1bd29f776aa11e73ad6d68ea1
ShahinAlam7893 <shahinalam7893@gmail.com>
2025-02-15 00:58:44 +0600

    OSTAD Code Mama Problem solve

--- M_23_01_Longest_Substring_Without_Repeating_Character.php
+++ M_23_01_Longest_Substring_Without_Repeating_Character.php
@@ -5,18 +5,15 @@ function longestSubstring($s) {
     $maxLength = 0;
     $start = 0;
 
-    // Iterate through the string
+
     for ($end = 0; $end < $n; $end++) {
-        // If the character is already in the map, update the start position
+
         if (isset($charIndexMap[$s[$end]])) {
-            // Move the start pointer to the next position of the previous duplicate character
             $start = max($start, $charIndexMap[$s[$end]] + 1);
         }
 
-        // Update the character's latest index in the map
         $charIndexMap[$s[$end]] = $end;
 
-        // Calculate the maximum length of substring without repeating characters
         $maxLength = max($maxLength, $end - $start + 1);
     }
 
@@ -25,6 +22,5 @@ function longestSubstring($s) {
 
 $inputString = trim(fgets(STDIN));
 
-// Call the function and display the result
 echo longestSubstring($inputString) . "\n";
 
